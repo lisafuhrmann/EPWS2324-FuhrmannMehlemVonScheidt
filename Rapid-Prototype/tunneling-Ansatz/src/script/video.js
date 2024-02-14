f51657d118651a3d1bfc56735b49f8e7eecc802a
Niklas Mehlem <74181767+NiklasMehlem@users.noreply.github.com>
2024-02-14 21:06:53 +0100

    + Audio Wiedergabe und QRCode ; (#75)
    + Code ist in der Lage Audio Datein abzuspielen und Synchron zu steuern ;
    + Erzeugt einen QRCode der PublicUrl |
    * Interkationen mit Medien sind nun PUT stat GET requests ;

--- Rapid-Prototype/tunneling-Ansatz/src/script/video.js
+++ Rapid-Prototype/tunneling-Ansatz/src/script/video.js
@@ -1,8 +1,51 @@
+const audio = document.getElementById('audios')
 const video = document.getElementById('videos')
 let letsPlay = false
 let letsPause = false
 let adminPlay = false
 let adminPause = false
+
+audio.addEventListener('play', function () {
+    if (!adminPlay) {
+        console.log("Trying to play Video... this is your playPermit: " + JSON.stringify(letsPlay))
+        if (!letsPlay) {
+            adminPause = true;
+            audio.pause();
+            console.log("Testing Code...")
+
+            const requestOptions = {
+                "method": "PUT",
+                "mode": "cors",
+                "redirect": "follow"
+            };
+
+            const currentTime = audio.currentTime;
+            const neatTime = currentTime.toFixed(4).substring(0, 5);
+            console.log("Current Time: " + neatTime)
+            fetch(cacheServiceEndpoint + "audioStart" + "?sessionKey=" + sessionKey + "&print=" + print + "&time=" + neatTime, requestOptions)
+        } else letsPlay = false
+    } else adminPlay = false
+});
+
+audio.addEventListener('pause', function () {
+    if (!adminPause) {
+        console.log("Trying to pause Video... this is your playPermit: " + JSON.stringify(letsPause))
+        if (!letsPause) {
+            adminPlay = true;
+            audio.play();
+            console.log("Testing Code...")
+
+            const requestOptions = {
+                "method": "PUT",
+                "mode": "cors",
+                "redirect": "follow"
+            };
+
+            fetch(cacheServiceEndpoint + "audioPause" + "?sessionKey=" + sessionKey + "&print=" + print, requestOptions)
+        } else letsPause = false
+    } else adminPause = false
+});
+
 
 video.onplay = () => {
     if (!adminPlay) {
@@ -13,13 +56,13 @@ video.onplay = () => {
             console.log("Testing Code...")
 
             const requestOptions = {
-                "method": "GET",
+                "method": "PUT",
                 "mode": "cors",
                 "redirect": "follow"
             };
 
             const currentTime = video.currentTime;
-            const neatTime = currentTime.toFixed(4).substring(0,5);
+            const neatTime = currentTime.toFixed(4).substring(0, 5);
             console.log("Current Time: " + neatTime)
             fetch(cacheServiceEndpoint + "videoStart" + "?sessionKey=" + sessionKey + "&print=" + print + "&time=" + neatTime, requestOptions)
         } else letsPlay = false
@@ -35,7 +78,7 @@ video.onpause = () => {
             console.log("Testing Code...")
 
             const requestOptions = {
-                "method": "GET",
+                "method": "PUT",
                 "mode": "cors",
                 "redirect": "follow"
             };
@@ -45,3 +88,10 @@ video.onpause = () => {
     } else adminPause = false
 
 }
+
+/*
+video.addEventListener('seeked', function() {
+    // Handle the 'seeked' event here
+    console.log('User skipped to a new position:', video.currentTime);
+});
+*/
